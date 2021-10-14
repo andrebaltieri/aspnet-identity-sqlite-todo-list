cd3710801637720f92313775d93e84916e0e9a37
Andre Baltieri <andrebaltieri@hotmail.com>
2021-10-14 10:33:06 -0300

    Add localization e tema

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace MyTodoApp.Models
@@ -7,14 +8,20 @@ namespace MyTodoApp.Models
     {
         public int Id { get; set; }
 
+        [DisplayName("Título")]
         [Required(ErrorMessage = "Campo obrigatório")]
         public string Title { get; set; }
 
+        [DisplayName("Concluído")]
         public bool Done { get; set; }
 
+        [DisplayName("Criado em")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+        [DisplayName("Última atualização")]
         public DateTime LastUpdateDate { get; set; } = DateTime.Now;
 
+        [DisplayName("Usuário")]
         public string User { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using MyTodoApp.Data;
 using Microsoft.Extensions.Configuration;
@@ -51,10 +53,19 @@ namespace MyTodoApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            var supportedCultures = new[] {new CultureInfo("pt-BR")};
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pt-BR", "pt-BR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
 
             app.UseAuthentication();
             app.UseAuthorization();
